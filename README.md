d7775d39db99b9c46d881f35a6019b49ec6ce825
Manuela Mendoza Barba <mbmendoza91@gmail.com>
2026-01-29 01:27:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Handmade Shop – Backend
+
+Backend de una plataforma e-commerce para productos artesanales.
+
+## Tecnologías
+- Java 21
+- Spring Boot
+- Maven
+- PostgreSQL
---
+++ src/test/java/com/handmade/ecommerce/backend/HandmadeShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handmade.ecommerce.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandmadeShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
