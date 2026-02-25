c7fce698827a26eaac764adea9eed6329da21c53
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-02-26 00:58:20 +0100

    Add application services for users, products and categories

--- src/main/java/com/handmade/ecommerce/backend/application/CategoryService.java
+++ src/main/java/com/handmade/ecommerce/backend/application/CategoryService.java
@@ -9,16 +9,21 @@ public class CategoryService {
     public CategoryService(ICategoryRepository iCategoryRepository) {
         this.iCategoryRepository = iCategoryRepository;
     }
+
     public Category save (Category category){
         return iCategoryRepository.save(category);
     }
+
     public Iterable<Category> findAll(){
         return iCategoryRepository.findAll();
     }
+
     public  Category findById(Integer id){
         return iCategoryRepository.findById(id);
     }
+
     public void deleteById(Integer id){
         iCategoryRepository.deleteById(id);
     }
+
 }
