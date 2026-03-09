15d83e7afd3d63d9f2df4f057675c60335608e71
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-03-09 19:32:31 +0100

    Expose REST endpoints for User operations

--- src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/UserController.java
+++ src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
         return userService.save(user);
     }
 
-    @GetMapping
+    @GetMapping("/{id}")
     public User findById(@PathVariable Integer id) {
         return userService.findById(id);
     }
