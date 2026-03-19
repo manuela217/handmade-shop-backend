58a2ca270b47d9f3955a4ed3b0b8d49a86924865
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-03-19 17:45:33 +0100

    Refactor CategoryController to use ResponseEntity and proper HTTP status codes

--- src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/CategoryController.java
+++ src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/CategoryController.java
@@ -3,6 +3,8 @@ package com.handmade.ecommerce.backend.infrastructure.rest;
 import com.handmade.ecommerce.backend.application.CategoryService;
 import com.handmade.ecommerce.backend.domain.model.Category;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,22 +18,23 @@ public class CategoryController {
     }
 
     @PostMapping
-    public Category save(@RequestBody Category category) {
-        return categoryService.save(category);
+    public ResponseEntity<Category> save(@RequestBody Category category) {
+        return new ResponseEntity<>(categoryService.save(category), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public Iterable<Category> findAll() {
-        return categoryService.findAll();
+    public ResponseEntity<Iterable<Category>> findAll() {
+        return ResponseEntity.ok(categoryService.findAll());
     }
 
     @GetMapping("/{id}")
-    public Category findById(@PathVariable Integer id) {
-        return categoryService.findById(id);
+    public ResponseEntity<Category> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok(categoryService.findById(id));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Integer id) {
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable Integer id) {
         categoryService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
