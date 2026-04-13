c1c0feab1da53bcdd417e060604b5f0d85bd0b99
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-04-13 18:04:28 +0200

    Add Order and OrderProduct mappers using MapStruct

---
+++ src/main/java/com/handmade/ecommerce/backend/infrastructure/mapper/IOrderMapper.java
@@ -0,0 +1,27 @@
+package com.handmade.ecommerce.backend.infrastructure.mapper;
+
+import com.handmade.ecommerce.backend.domain.model.Order;
+import com.handmade.ecommerce.backend.infrastructure.entity.OrderEntity;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring", uses = {IOrderProductMapper.class})
+public interface IOrderMapper {
+    @Mappings(
+            {
+                    @Mapping(source = "id", target = "id"),
+                    @Mapping(source = "dateCreated", target = "dateCreated"),
+                    @Mapping(source = "orderProducts", target = "orderProducts"),
+                    @Mapping(source = "state", target = "state"),
+                    @Mapping(source = "userEntity.id", target = "userId")
+            }
+    )
+    Order toOrder(OrderEntity orderEntity);
+    Iterable<Order> toOrderList(Iterable<OrderEntity> orderEntities);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "userEntity", ignore = true)
+    OrderEntity toOrderEntity(Order order);
+}
