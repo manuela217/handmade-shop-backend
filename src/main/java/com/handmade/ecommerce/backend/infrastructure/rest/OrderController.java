ec798c5bcdc92b59d43a624967cb16b5a2d927e6
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-04-15 01:32:31 +0200

    Add Order REST controller with endpoints for create, update state and list

---
+++ src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/OrderController.java
@@ -0,0 +1,35 @@
+package com.handmade.ecommerce.backend.infrastructure.rest;
+
+import com.handmade.ecommerce.backend.application.OrderService;
+import com.handmade.ecommerce.backend.domain.model.Order;
+import com.handmade.ecommerce.backend.domain.model.OrderState;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/orders")
+@Slf4j
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> save(@RequestBody Order order) {
+        return ResponseEntity.ok(orderService.save(order));
+    }
+
+    @PutMapping("/{id}/state")
+    public ResponseEntity<Void> updateStateById(@PathVariable Integer id, @RequestParam String state) {
+        orderService.updateStateById(id, state);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Order>> findAll() {
+        return ResponseEntity.ok(orderService.findAll());
+    }
+}
