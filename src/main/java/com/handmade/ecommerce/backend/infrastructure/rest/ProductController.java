226518ed354ca7c221e8f55c2e868d99ba39cc93
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-05-15 18:08:53 +0200

    Update product creation endpoint to handle multipart form data

--- src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/ProductController.java
+++ src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/ProductController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+
 @RestController
 @RequestMapping("/api/v1/admin/products")
 @Slf4j
@@ -19,7 +21,27 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> save(@RequestBody Product product) {
+    public ResponseEntity<Product> save(
+            @RequestParam("id") Integer id,
+            @RequestParam("code") String code,
+            @RequestParam("name") String name,
+            @RequestParam("description") String description,
+            @RequestParam("price") BigDecimal price,
+            @RequestParam("urlImage") String urlImage,
+            @RequestParam("userId") Integer userId,
+            @RequestParam("categoryId") Integer categoryId
+            ) {
+
+        Product product = new Product();
+        product.setId(id);
+        product.setCode(code);
+        product.setName(name);
+        product.setDescription(description);
+        product.setPrice(price);
+        product.setCategoryId(categoryId);
+        product.setUserId(userId);
+        product.setUrlImage(urlImage);
+
         log.info("Nombre producto: {}", product.getName());
         return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
