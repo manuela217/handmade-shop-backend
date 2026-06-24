bf31d9343f2d25e33915a246a7dc78091ab4a22a
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-06-24 19:06:45 +0200

    Enable CORS for order  controller

--- src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/OrderController.java
+++ src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/OrderController.java
@@ -7,7 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/orders")
+@RequestMapping("/api/v1/orders")
+@CrossOrigin(origins = "http://localhost:4200")
 @Slf4j
 public class OrderController {
     private final OrderService orderService;
