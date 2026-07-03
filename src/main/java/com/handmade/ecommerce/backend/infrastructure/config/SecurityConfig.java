84b996d343cd42c1ce1fd0da1f29838f92cd06a3
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-07-03 18:30:27 +0200

    Add AuthenticationManager bean configuration

--- src/main/java/com/handmade/ecommerce/backend/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/handmade/ecommerce/backend/infrastructure/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.handmade.ecommerce.backend.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +12,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
