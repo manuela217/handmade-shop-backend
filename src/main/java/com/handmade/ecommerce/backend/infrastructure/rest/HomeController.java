8d86ace0e568793e0dd4c988d8fd65574d7309ff
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-07-27 01:02:02 +0200

    Enable cross-origin requests for public controllers

--- src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/HomeController.java
+++ src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/HomeController.java
@@ -3,12 +3,14 @@ package com.handmade.ecommerce.backend.infrastructure.rest;
 import com.handmade.ecommerce.backend.application.ProductService;
 import com.handmade.ecommerce.backend.domain.model.Product;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/home")
+@CrossOrigin(origins = "http://localhost:4200")
 public class HomeController {
     private final ProductService productService;
 
--- src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/RegistrationController.java
+++ src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/RegistrationController.java
@@ -4,13 +4,11 @@ import com.handmade.ecommerce.backend.application.RegistrationService;
 import com.handmade.ecommerce.backend.domain.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/security")
+@CrossOrigin(origins = "http://localhost:4200")
 public class RegistrationController {
     private final RegistrationService registrationService;
 
