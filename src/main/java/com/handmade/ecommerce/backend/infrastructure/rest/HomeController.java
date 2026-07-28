301593e7a0a3a84d7585b7759e5ead59c212d300
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-07-28 14:15:50 +0200

    Add public endpoint for product details

--- src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/HomeController.java
+++ src/main/java/com/handmade/ecommerce/backend/infrastructure/rest/HomeController.java
@@ -3,10 +3,7 @@ package com.handmade.ecommerce.backend.infrastructure.rest;
 import com.handmade.ecommerce.backend.application.ProductService;
 import com.handmade.ecommerce.backend.domain.model.Product;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/home")
@@ -21,5 +18,10 @@ public class HomeController {
     @GetMapping
     public ResponseEntity<Iterable<Product>> findAll() {
         return ResponseEntity.ok(productService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok(productService.findById(id));
     }
 }
