dec4e1ff1b7b2ff27943b0677800bd466376e941
Manuela Mendoza Barba <manuela.mendoza.dev@gmail.com>
2026-08-21 16:49:04 +0200

    Remove unused imports and debug code from application

--- src/main/java/com/handmade/ecommerce/backend/infrastructure/adapter/IUserCrudRepository.java
+++ src/main/java/com/handmade/ecommerce/backend/infrastructure/adapter/IUserCrudRepository.java
@@ -2,7 +2,6 @@ package com.handmade.ecommerce.backend.infrastructure.adapter;
 
 import com.handmade.ecommerce.backend.infrastructure.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
-
 import java.util.Optional;
 
 public interface IUserCrudRepository extends CrudRepository<UserEntity, Integer> {
